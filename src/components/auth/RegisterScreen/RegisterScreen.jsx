21008a5bb4929dbe2dde7e7c054ff7dc015033f1
DreadfulCrazy <lucasospina97@gmail.com>
2021-09-14 18:38:44 -0500

    Register screen

--- src/components/auth/RegisterScreen/RegisterScreen.jsx
+++ src/components/auth/RegisterScreen/RegisterScreen.jsx
@@ -1,10 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const RegisterScreen = () => {
   return (
-    <div>
-      <h1>Register Screen</h1>
-    </div>
+    <>
+      <h3 className="auth__tittle">Login</h3>
+      <form action="">
+        <input
+          type="text"
+          placeholder="email"
+          name="email"
+          className="auth__input"
+          autoComplete="off"
+        />
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          className="auth__input"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          className="auth__input"
+        />
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          name="password2"
+          className="auth__input"
+        />
+        <button type="submit" className="btn btn-primary btn-block mb-5">
+          Login
+        </button>
+        <hr />
+      </form>
+      <Link to="/auth/login" className="link">
+        Already registered?
+      </Link>
+    </>
   );
 };
 
